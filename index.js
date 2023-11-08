0853fc77eb243f7962b5456baa6c8a7b95905ca2
zdeauS <c0880116@mylambton.ca>
2023-11-08 17:00:17 +0000

    added JSON responses and made various edits to index.js and productroutes.js

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 // variable declaration
-const express = require('express');
-import mongoose from "mongoose";
-const bodyParser = require('body-parser');
+import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
-mongoose.connect('mongodb://localhost/my_db');
-
-let productSchema = mongoose.Schema({
-   Name: String,
-   Price: Number,
-   Colour: String,
-   Manufacturer: String,
-   StartingDateAvailable: Date,
-   StartingDateAvailable: Date,
-   Type: String,
-   PanWidth: Number,
-   HandleLength: Number,
-   Image: String,
-   Description: String
-});
-
-let Product = mongoose.model("Product", productSchema);
-
+// import product routes
 import products from "./productroutes.js";
 
+// make the public folder static
 app.use(express.static('public'));
+app.use(express.static('public/images'));
 
+// use the products route
 app.use('/Products', products);
 
+// listen
 app.listen(8080);
--- productroutes.js
+++ productroutes.js
@@ -1,24 +1,63 @@
+// variable declaration
 import express from "express";
+import mongoose from "mongoose";
 const products = express.Router();
 
+// connects to mongodb
+mongoose.connect('mongodb://localhost/ProductCatalog');
+
+// create schema
+let productSchema = mongoose.Schema({
+   Name: String,
+   Price: Number,
+   Colour: String,
+   Manufacturer: String,
+   StartingDateAvailable: Date,
+   StartingDateAvailable: Date,
+   Type: String,
+   PanWidth: Number,
+   HandleLength: Number,
+   Image: String,
+   Description: String
+});
+
+// assign schema to a variable
+let Product = mongoose.model("Product", productSchema);
+
+// return all products as a JSON response
 products.get('/', (req, res) => {
-	
+	Product.find(function(err, response) {
+      res.json(response);
+	  });
 });
 
+// return id of all products as a JSON response
 products.get('/identifiers', (req, res) => {
-	
+	Product.find("id", function(err, response) {
+      res.json(response);
+	  });
 });
 
+// return products with specified id as a JSON response
 products.get('/:id', (req, res) => {
-	
+	Product.findById(req.params.id, function(err, response) {
+      res.json(response);
+	  });
 });
 
+// return specified fields of products with specified id as a JSON response
 products.get('/:id/:field', (req, res) => {
-	
+	Product.findById(req.params.id, req.params.field, function(err, response) {
+      res.json(response);
+	  });
 });
 
+// return images specified by filename
 products.get('/images/:filename', (req, res) => {
-	
+	Product.find(req.params.filename, function(err, response) {
+      res.json(response);
+	  });
 });
 
+// export route
 export default products;
